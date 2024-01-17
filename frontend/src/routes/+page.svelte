83d3a05fe1adcaf3ea57152456195a5e24c4fabb
daniil7 <daniil.k@mail.ru>
2024-01-17 06:10:03 +0500

    add skeleton styles

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="main">
+
+    <h1 style="text-align: center;"> Course Aspects Analyser </h1>
+
+    <hr />
+
+
+
+</div>
