d57c85facd89a1ce0792384bee10993b3ff54419
Albert Fazlyev <albert.fz@yandex.ru>
2024-03-04 23:24:52 +0500

    glue things together

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Body
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from pydantic import Field
+from pydantic import BaseModel, Field
+from functools import lru_cache
+from typing import Annotated, Callable, Optional
+from sentence_transformers import SentenceTransformer
+from torch import Tensor, nn
 
-from backend.settings import settings
-from ai.search import MethodSimilarity
-from ai.emotional import Emotional
+
+from ai.sentiaspect_evaluation import (
+    make_sentiaspect_evaluator, 
+    avg_evaluation_strategy,
+    AspectRating
+)
+from ai.text_segmentation import sentence_segmentizer
+from ai.aspect_classification import make_embeds_sim_classifier
+from ai.sentiment_analysis import make_hf_sentiment_analyzer, Sentiment
+
 
 
 app = FastAPI()
@@ -22,57 +32,57 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-search = MethodSimilarity()
-emotional = Emotional()
+@lru_cache
+def get_embeddings_model() -> Callable[[str], Tensor]:
+    model = SentenceTransformer('cointegrated/rubert-tiny2')
+    def encode(s: str) -> Tensor:
+        return model.encode(s, convert_to_tensor=True) # type: ignore
+    
+    return encode
 
-default_aspects = search.aspects_list
+@lru_cache
+def get_aspect_classifier(
+    embeddings_model: Annotated[
+        Callable[[str], Tensor],
+        Depends(get_embeddings_model),
+        "The function that returns sentence embeddings."
+    ],
+    aspect_labels: Annotated[list[str], Body(description="Список слов, отражающих тот или иной аспект.")]
+) -> Callable[[str], Optional[str]]:
+    return make_embeds_sim_classifier(aspect_labels, embeddings_model, 0.3, nn.CosineSimilarity())
 
-class Request(BaseModel):
-    text: str = Field(description="Текст одного или нескольких отзывов.")
-    aspects_list: list|None = Field(default=None, description="Опциональный параметр, список слов, отражающих тот или иной аспект.")
+
+@lru_cache
+def get_sentiment_analyzer() -> Callable[[str], Sentiment]:
+    return make_hf_sentiment_analyzer('cointegrated/rubert-tiny2-sentiment')
+
+
+@lru_cache
+def get_sentiaspect_evaluator(
+    aspect_classifier: Annotated[
+        Callable[[str], str],
+        Depends(get_aspect_classifier),
+    ],
+    sentiment_analyzer: Annotated[
+        Callable[[str], Sentiment],
+        Depends(get_sentiment_analyzer),
+    ]
+) -> Callable[[str], AspectRating]:
+    return make_sentiaspect_evaluator(
+        sentence_segmentizer, aspect_classifier, sentiment_analyzer, avg_evaluation_strategy
+    )
+
+
 
 @app.post("/")
-async def root(item: Request):
+async def root(
+    text: Annotated[str, Body(description="Текст отзыва")],
+    sentiaspect_evaluator: Annotated[
+        Callable[[str], AspectRating],
+        Depends(get_sentiaspect_evaluator),
+    ]
+) -> AspectRating:
     """
-    Маршрут, выполняющий поиск аспектов и оценку их эмоциональной окраски
-
-    Формат входных данных:
-
-    ```
-    {
-      "text": "<текст>" # Текст одного или нескольких отзывов.
-      "aspects_list": [...] # Опциональный параметр, список слов, отражающих тот или иной аспект.
-    }
-    ```
-
-    Формат выходных данных:
-
-    ```
-    {
-      "<аспект>": { # строка, относящаяся к аспекту.
-        "score": <число>, # общая оценка эмоциональной окраски данного аспекта во всех отзывах [0, 1].
-        "positive": <число>, # количество фрагментов, содержащих упоминание аспекта, с положительной эмоциональной окраской.
-        "negative": <число>, # количество фрагментов, содержащих упоминание аспекта, с негативной эмоциональной окраской.
-        "neutral": <число>, # количество фрагментов, содержащих упоминание аспекта, с нейтральной эмоциональной окраской.
-        "total": <число> # общее количество фрагментов, содержащих упоминание аспекта.
-      },
-      ...
-    }
-    ```
+    Аспектно-сентиментный анализ отзыва.
     """
-    text = item.text
-    if item.aspects_list:
-        search.set_aspects(item.aspects_list)
-    else:
-        search.set_aspects(default_aspects)
-    result = {}
-    all_aspects = search.process(text, 0.3)
-    for aspect, sentences in all_aspects.items():
-        if len(sentences) == 0:
-            continue
-        result[aspect] = {"score": 0, "positive": 0, "negative": 0, "neutral": 0, "total": len(sentences)}
-        for sentence in sentences:
-            predict = emotional.predict(sentence)
-            result[aspect][predict.lower()] += 1
-        result[aspect]["score"] = result[aspect]["positive"] / result[aspect]["total"] - result[aspect]["negative"] / result[aspect]["total"]
-    return result
+    return sentiaspect_evaluator(text)
